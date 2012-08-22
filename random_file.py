4c77e412af4223c9f7d3a37aa1f5ad34be079f2c
Jean-Francois Monfort <jf.monfort@gmail.com>
2012-08-22 21:16:17 +0200

    consider sub directories

--- random_file.py
+++ random_file.py
@@ -3,7 +3,17 @@
 import os, subprocess, random
 
 path = "/mnt/movies/"
-filepath = path + random.choice(os.listdir(path))
+
+# TODO use a recursive function
+files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+dirs = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+
+for subdir in dirs:
+	for f in os.listdir(os.path.join(path, subdir)):
+		if os.path.isfile(os.path.join(path, subdir, f)):
+			files.append(os.path.join(subdir, f))
+
+filepath = os.path.join(path, random.choice(files))
 
 if os.name == 'mac':
     subprocess.call(('open', filepath))
