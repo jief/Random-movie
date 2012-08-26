dfdbf506ccbb29cedd6596736b3a1100be734acb
Jean-Francois Monfort <jf.monfort@gmail.com>
2012-08-26 12:31:33 +0200

    Use a recursive function to collect all files

--- random_file.py
+++ random_file.py
@@ -2,18 +2,24 @@
 
 import os, subprocess, random
 
-path = "/mnt/movies/"
+path = "/mnt/movies"
 
-# TODO use a recursive function
 files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 dirs = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 
-for subdir in dirs:
-	for f in os.listdir(os.path.join(path, subdir)):
+def get_files(path, subdir):
+  for f in os.listdir(os.path.join(path, subdir)):
 		if os.path.isfile(os.path.join(path, subdir, f)):
 			files.append(os.path.join(subdir, f))
+		elif os.path.isdir(os.path.join(path, subdir, f)):
+			files + get_files(path, os.path.join(subdir, f))
+  return files
+
+for subdir in dirs:
+  files + get_files(path, subdir)
 
 filepath = os.path.join(path, random.choice(files))
+print filepath
 
 if os.name == 'mac':
     subprocess.call(('open', filepath))
